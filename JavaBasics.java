467924e5fa7fb32de90ffd00774a498768032b61
knightking22 <155818251+knightking22@users.noreply.github.com>
2024-01-08 23:13:36 -0800

    Delete JavaBasics.java

--- JavaBasics.java
+++
@@ -1,8 +0,0 @@
-public class JavaBasics {
-    public static void printHelloWorld() {
-        System.out.println("Hello World");
-    }
-public static void main(String[] args) {
-    printHelloWorld();
-}
-}
