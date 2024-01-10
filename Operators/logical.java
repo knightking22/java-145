22c1bf1d678315dfb471202c8ee15ed124f2df47
knightking22 <155818251+knightking22@users.noreply.github.com>
2024-01-09 19:44:01 -0800

    Add files via upload

---
+++ Operators/logical.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class logical {
+    public static void main(String args[]) {
+        
+        //logical And 
+        System.out.println( (3>2) && (5<0));   
+
+         //Logical OR
+         System.out.println( (3>2) || (5<0));  
+
+          //Logical Not
+          System.out.println( !(3>2));       
+    }
+    
+}
